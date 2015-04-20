2943e11fb7156d995f502e0afc11d41b0d98b2e1
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-20 12:25:17 -0400

    Added a TableViewController with a NavController

--- TableViewByCode/AppDelegate.m
+++ TableViewByCode/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FeedTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FeedTableViewController *feedTableVC = [[FeedTableViewController alloc]init];
+    UINavigationController *feedTableNav = [[UINavigationController alloc]initWithRootViewController:feedTableVC];
+    
     return YES;
 }
 
